9c9964b6dd07c61c008e37817c7753109e79a935
YJ <145392731+LeBronzeAge@users.noreply.github.com>
2026-07-01 21:21:53 +0100

    Completed day 1 exercises, added README for day 1,added gitignore

---
+++ day-1/README.md
@@ -0,0 +1,9 @@
+## Day 1 Work
+
+- Learned some terminal commands because I want to work mainly through the terminal.
+- Practised key file and folder commands.
+- Installed GitHub CLI (`gh`) via Homebrew so I can create GitHub repositories directly from the terminal.
+- Learned key `git` and `gh` commands for terminal-based workflow.
+- Created this repository and started Day 1 of the challenge.
+- Played around with the Python shell.
+- Completed the Day 1 exercises.
---
+++ day-1/helloworld.py
@@ -0,0 +1,58 @@
+# Day 1 - 30DaysOfPython Challenge
+
+# Exercise: Level 2
+
+#1
+import sys
+print(sys.version)
+
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#3
+print("Yuxuan")
+print("Jiang")
+print("England")
+print("I am enjoying 30 days of python")
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Yuxuan"))
+print(type("Jiang"))
+print(type("England"))
+
+#Exercise: Level 3
+
+#1
+a = 5
+b = 3.4
+c = 3 + 4j
+d = 'Hello, World!'
+e = True
+f = [1, 2, 3, 4, 5]
+g = (1, 2, 3, 4, 5)
+h = {1, 2, 3, 4, 5}
+i = {'name': 'Yuxuan', 'age': 20, 'country': 'England'}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+
+#2
+x = ((10 - 2)**2 + (8 - 3)**2) ** 0.5
+print(x)
